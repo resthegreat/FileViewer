ad6a4d8dbe57d0b77756e70c5b93478b2bb61c47
resthegreat <39837674+resthegreat@users.noreply.github.com>
2019-04-08 11:02:55 -0400

    Added program files

---
+++ FileExplorer.java
@@ -0,0 +1,75 @@
+package com.resmagnus.fileexplorer;
+
+import java.awt.*;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class FileExplorer {
+
+    private ArrayList<File> directoryStack;
+    private File[] currentDirectoryFiles;
+
+    public FileExplorer() {
+
+        directoryStack = new ArrayList<>();
+
+        if(OSValidator.isWindows()) {
+            addDirectory(new File(System.getenv("SystemDrive") + "//"));
+        }
+
+        else if(OSValidator.isUnix()) {
+            addDirectory(new File("/"));
+        }
+    }
+
+    public void printCurrentDirectoryFiles() {
+        for(int i = 0; i < currentDirectoryFiles.length; i++) {
+            System.out.println(i + " " + currentDirectoryFiles[i].getName());
+        }
+        System.out.println("\n");
+    }
+
+    public void addDirectory(File dir) {
+        if(dir.isDirectory()) {
+            directoryStack.add(dir);
+            currentDirectoryFiles = currentDirectoryFilesToArray();
+        }
+        else {
+            System.out.println("Error: Selected file is not a directory.\n");
+        }
+    }
+
+    public void removeLastDirectory() {
+        if(directoryStack.size() != 1) {
+            directoryStack.remove(directoryStack.get(directoryStack.size() - 1));
+            currentDirectoryFiles = currentDirectoryFilesToArray();
+        }
+    }
+
+    public File[] currentDirectoryFilesToArray() {
+        File[] s = directoryStack.get(directoryStack.size() - 1).listFiles();
+        if(s != null) {
+            return s;
+        }
+
+        else {
+            removeLastDirectory();
+            System.out.println("Error: You do not have permission to view the contents of this folder.\n");
+            return s = directoryStack.get(directoryStack.size() - 1).listFiles();
+        }
+    }
+
+    public void openFile(File file) {
+        if(file.isFile()) {
+            try {
+                Desktop.getDesktop().open(file);
+            }catch(IOException e) {
+                System.out.println("Error: Could not open file.\n");
+            }
+        }
+        else {
+            System.out.println("Error: Selected file is not a file.\n");
+        }
+    }
+}
---
+++ Main.java
@@ -0,0 +1,80 @@
+package com.resmagnus.fileexplorer;
+
+import java.io.File;
+import java.util.Scanner;
+
+public class Main {
+
+    private static Scanner keyboard;
+
+    public static void main(String[] args) {
+
+        FileExplorer fileExplorer = new FileExplorer();
+        keyboard = new Scanner(System.in);
+        boolean loop = true;
+
+        while(loop) {
+            String userIndex;
+            int userIndexInt;
+            String option;
+
+            fileExplorer.printCurrentDirectoryFiles();
+
+            printMenu();
+
+            System.out.print("Please choose an option: ");
+            option = keyboard.nextLine();
+
+            switch(option) {
+
+                case "0" :
+                    File[] s = fileExplorer.currentDirectoryFilesToArray();
+                    System.out.print("Enter an index: ");
+                    userIndex = keyboard.nextLine();
+                    try {
+                        userIndexInt = Integer.parseInt(userIndex);
+                        fileExplorer.addDirectory(s[userIndexInt]);
+                    } catch(NumberFormatException e) {
+                            System.out.println("Error: Entered data is not an index.\n");
+                    }
+                    System.out.println();
+                    break;
+
+                case "1" :
+                    fileExplorer.removeLastDirectory();
+                    System.out.println();
+                    break;
+
+                case "2" :
+                    s = fileExplorer.currentDirectoryFilesToArray();
+                    System.out.print("Enter an d" +
+                            "index: ");
+                    userIndex = keyboard.nextLine();
+                    try {
+                        userIndexInt = Integer.parseInt(userIndex);
+                        fileExplorer.openFile(s[userIndexInt]);
+                    }catch (NumberFormatException e) {
+                        System.out.println("Error: Entered data is not an index.\n");
+                    }
+                    System.out.println();
+                    break;
+
+                case "3" :
+                    loop = false;
+                    break;
+
+                default :
+                    System.out.println();
+                    break;
+            }
+        }
+    }
+
+    public static void printMenu() {
+        System.out.println("/////////////MENU////////////\n");
+        System.out.println("(0) Go into a folder");
+        System.out.println("(1) Open a file");
+        System.out.println("(2) Go back");
+        System.out.println("(3) Quit the program\n");
+    }
+}
